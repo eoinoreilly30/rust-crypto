aa0e7dca7bbae4dafcbb170b1f7f7d15a8ea67b5
eoin <eoinoreilly30@gmail.com>
2021-11-11 13:32:42 +0000

    Add sha256_hash function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
-struct Transaction {
-    to: str,
-    from: str,
-    amount: f64
-}
+use sha2::{Sha256, Digest};
 
-struct Block {
-    previous_hash: str,
-    transaction: Transaction
-}
+// struct Transaction {
+//     to: String,
+//     from: String,
+//     amount: f64,
+// }
+//
+// struct Block {
+//     previous_hash: String,
+//     transaction: Transaction,
+// }
+//
+// struct Wallet {
+//     public_key: String,
+//     private_key: String,
+// }
 
-struct Wallet {
-    public_key: str,
-    private_key: str
+fn sha256_hash(data: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(data);
+    hex::encode(hasher.finalize())
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}", sha256_hash(&"Hello"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sha256_hash_empty() {
+        assert_eq!(sha256_hash(&""), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+    }
+
+    #[test]
+    fn sha256_hash_hello() {
+        assert_eq!(sha256_hash(&"Hello"), "185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969")
+    }
 }
